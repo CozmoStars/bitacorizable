f3a09cfc27be690ffa52cd24f1573d2cc2e7b3ce
Said Antonio Guerrero García <saidguerrerogd@gmail.com>
2025-04-12 03:21:47 -0600

    Se agrega personalizacion de base de datos para llave foranea de user_id
    Se agrega personalizacion de base de datos para llave foranea de user_id

--- src/BitacorizableServiceProvider.php
+++ src/BitacorizableServiceProvider.php
@@ -8,7 +8,10 @@ class BitacorizableServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        // Carga migraciones
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        // Permite publicar la migración
+        $this->publishes([
+            __DIR__ . '/database/migrations/create_bitacoras_table.php.stub' =>
+                database_path('migrations/' . date('Y_m_d_His', time()) . '_create_bitacoras_table.php'),
+        ], 'bitacorizable-migrations');
     }
 }
